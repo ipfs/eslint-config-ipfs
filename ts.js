ccf3b8870b0371d7099733b0c467fb534a90325d
Irakli Gozalishvili <contact@gozala.io>
2020-09-14 20:06:03 -0700

    fix: allow use before defs in TS

--- ts.js
+++ ts.js
@@ -7,4 +7,8 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   extends: ["./js", "plugin:@typescript-eslint/recommended"],
+  rules: {
+    // Types often are recursive & no use before define is too restrctive
+    "no-use-before-define": 0
+  }
 }
