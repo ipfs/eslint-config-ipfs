932141cc1b32b418a9148db39f62e7827b88c6f3
achingbrain <alex@achingbrain.net>
2022-08-10 10:19:22 +0100

    chore: update eslint code

--- test/validate.js
+++ test/validate.js
@@ -2,14 +2,17 @@ const eslint = require("eslint")
 const test = require("tape")
 const eslintrc = require("..")
 
-test("load config in eslint to validate all rule syntax is correct", function (t) {
-  var cli = new eslint.CLIEngine({
+console.info(eslint)
+
+test("load config in eslint to validate all rule syntax is correct", async function (t) {
+  var cli = new eslint.ESLint({
     useEslintrc: false,
     baseConfig: eslintrc,
   })
 
   var code = "var foo = 1\nvar bar = function () {}\nbar(foo)\n"
+  var result = await cli.lintText(code)
 
-  t.equal(cli.executeOnText(code).errorCount, 0)
+  t.equal(result[0].errorCount, 0)
   t.end()
 })
